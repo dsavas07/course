9210da7b15ee1a23e37d26bd4554ec19bd1d7de2
Dan Savas <dsavas07@gmail.com>
2026-02-13 17:55:28 -0500

    dbt Udemy training course updates 02-13-2026

---
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,20 @@
+WITH src_listings AS (
+    SELECT * from {{ ref('src_listings') }}
+)
+SELECT
+  listing_id,
+  listing_name,
+  room_type,
+  CASE
+    WHEN minimum_nights = 0 THEN 1
+    ELSE minimum_nights
+  END AS minimum_nights,
+  host_id,
+  CAST(REPLACE(
+    price_str,
+    '$'
+  ) AS NUMBER(10,0)) AS price,
+  created_at,
+  updated_at
+FROM
+  src_listings
